108eee745d3c24052abe68b3dece67d03505e4e0
Andrea V <1577639+karimodm@users.noreply.github.com>
2024-06-09 16:05:04 +0200

    Remove unused old code

--- lib.go
+++ lib.go
@@ -1,7 +1,6 @@
 package typejuggle
 
 import (
-	"fmt"
 	"reflect"
 )
 
@@ -69,8 +68,6 @@ func assignStructFieldsRecursive(srcVal reflect.Value, destVal reflect.Value, co
 			fieldName = "ID"
 		}
 
-		fmt.Println(fieldName)
-
 		// Attempt to find the field in the destination struct
 		destFieldVal := destVal.FieldByName(fieldName)
 		if !destFieldVal.IsValid() {
@@ -79,95 +76,5 @@ func assignStructFieldsRecursive(srcVal reflect.Value, destVal reflect.Value, co
 		}
 
 		assignRecursive(srcFieldVal, destFieldVal, convert...)
-
-		// if destFieldVal.IsValid() && destFieldVal.CanSet() {
-		// 	if srcFieldVal.Kind() == reflect.Ptr && srcFieldVal.Elem().Kind() == reflect.Struct {
-		// 		if destFieldVal.Kind() == reflect.Ptr && destFieldVal.IsNil() {
-		// 			// Allocate memory for the destination field's type if its pointer is nil
-		// 			destFieldVal.Set(reflect.New(destFieldVal.Type().Elem()))
-
-		// 			// Skip if the destination field is not a struct but the source is
-		// 			if destFieldVal.Elem().Kind() != reflect.Struct {
-		// 				continue
-		// 			}
-		// 		}
-
-		// 		assignStructFieldsRecursive(srcFieldVal.Elem(), destFieldVal.Elem())
-		// 	} else if srcFieldVal.Kind() == reflect.Struct && destFieldVal.Kind() == reflect.Ptr && destFieldVal.Type().Elem().Kind() == reflect.Struct {
-		// 		if destFieldVal.IsNil() {
-		// 			destFieldVal.Set(reflect.New(destFieldVal.Type().Elem()))
-		// 		}
-		// 		assignStructFieldsRecursive(srcFieldVal, destFieldVal.Elem())
-		// 	} else if srcFieldVal.Kind() == reflect.Struct && destFieldVal.Kind() == reflect.Struct {
-		// 		assignStructFieldsRecursive(srcFieldVal, destFieldVal)
-		// 	} else if srcFieldVal.Kind() == reflect.Slice && destFieldVal.Kind() == reflect.Slice {
-		// 		assignSliceFields(srcFieldVal, destFieldVal)
-		// 	} else if srcFieldVal.Type().AssignableTo(destFieldVal.Type()) {
-		// 		destFieldVal.Set(srcFieldVal)
-		// 	} else if srcFieldVal.Type().ConvertibleTo(destFieldVal.Type()) {
-		// 		destFieldVal.Set(srcFieldVal.Convert(destFieldVal.Type()))
-		// 	} else if destFieldVal.Kind() == reflect.Ptr {
-		// 		if destFieldVal.IsNil() {
-		// 			destFieldVal.Set(reflect.New(destFieldVal.Type().Elem()))
-		// 		}
-
-		// 		if srcFieldVal.Type().AssignableTo(destFieldVal.Type().Elem()) {
-		// 			destFieldVal.Elem().Set(srcFieldVal)
-		// 		} else if srcFieldVal.Type().ConvertibleTo(destFieldVal.Type().Elem()) {
-		// 			destFieldVal.Elem().Set(srcFieldVal.Convert(destFieldVal.Type().Elem()))
-		// 		}
-
-		// 		if srcFieldVal.Kind() == reflect.Ptr {
-		// 			if srcFieldVal.Elem().Type().AssignableTo(destFieldVal.Type().Elem()) {
-		// 				destFieldVal.Elem().Set(srcFieldVal.Elem())
-		// 			} else if srcFieldVal.Elem().Type().ConvertibleTo(destFieldVal.Type().Elem()) {
-		// 				destFieldVal.Elem().Set(srcFieldVal.Elem().Convert(destFieldVal.Type().Elem()))
-		// 			}
-		// 		}
-		// 	}
-		// }
 	}
-}
-
-func main() {
-	type Inner struct {
-		Name string
-		ID   int
-	}
-
-	type InnerLow struct {
-		Name string
-		Id   int
-	}
-
-	type A struct {
-		Inner  Inner
-		Detail string
-	}
-
-	type B struct {
-		Inner  InnerLow
-		Detail string
-	}
-
-	a := B{Inner: InnerLow{Name: "Example", Id: 42}, Detail: "Detail A"}
-	b := A{}
-
-	aSlice := []B{a, a}
-	var bSlice []*A
-
-	fmt.Printf("Before assignment: %+v\n", b)
-	FillFields(a, &b)
-	fmt.Printf("After assignment: %+v\n", b)
-
-	fmt.Printf("Before slice assignment: %+v\n", bSlice)
-	FillFields(aSlice, &bSlice)
-	fmt.Printf("After slice assignment: %+v\n", bSlice)
-
-	fmt.Printf("Assignment: %+v\n", *bSlice[0])
-	aSlice[0].Inner.Name = "Changed"
-	bSlice[0].Inner.Name = "WOOP!"
-
-	fmt.Printf("Assignment: %+v\n", aSlice[0])
-	fmt.Printf("Assignment: %+v\n", *bSlice[0])
 }
